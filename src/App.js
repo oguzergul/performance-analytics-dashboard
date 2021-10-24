c9711eacac709d0b0e2ef1c33c83621ad3bc9aa5
oguzergul <erguloguz@hotmail.com>
2021-10-24 23:37:30 +0300

    [refactor]: main file refactored.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {Card, Container, Flex, Grid, Header, Text, TimeBlock} from "./components";
+import {Card, Container, Flex, Link, Grid, Header, Text, TimeBlock} from "./components";
 import {axiosInstance} from "./utils/axios";
 import Chart from "./components/Chart";
 
@@ -32,13 +32,14 @@ function App() {
         }).finally(() => {
             setLoader(false)
         });
-
-
     };
 
     const getLastHalfHoursMetrics = async () => {
         setLoader(true);
-        await axiosInstance('/').then(res => setAnalytics(res.data)).catch(err => {
+        await axiosInstance('/').then(res => {
+            setAnalytics(res.data);
+            console.log('res',res)
+        }).catch(err => {
             console.log(err)
         }).finally(() => {
             setLoader(false)
@@ -50,69 +51,59 @@ function App() {
         getLastHalfHoursMetrics();
     }, [])
 
+
+    const createChart = (data = []) => {
+        const labels = data.map((e) => new Date(e.measurement_date).getUTCDate());
+        return [
+            {title: 'TTFB', data: data.map((e) => e.ttfbTime), label: labels},
+            {title: 'TTFB', data: data.map((e) => e.fcpTime), label: labels},
+            {title: 'DOM LOAD TIME', data: data.map((e) => e.domLoadTime), label: labels},
+            {title: 'WINDOW LOAD TIME', data: data.map((e) => e.windowLoadTime), label: labels},
+        ]
+    }
+
     return (
-        <div>
+        <div className={'pb-12'}>
             <Header/>
             <Container>
-                <TimeBlock onDateSubmit={getAnalyticsBetweenRange} getFromTime={getMinSelectedDate}
-                           getToTime={getMaxSelectedDate}/>
 
-                <Grid lg={2} sm={1} direction={'column'} gap={4}>
+                <TimeBlock
+                    onDateSubmit={getAnalyticsBetweenRange}
+                    getFromTime={getMinSelectedDate}
+                    getToTime={getMaxSelectedDate}
+                />
+
+
+                {analytics.length > 0 ? <Grid lg={2} sm={1} direction={'column'} gap={4}>
+                        {createChart(analytics).map((e, index) =>
+                            <Card key={index}>
+                                <Flex layouts={['col']} justify={['center']}
+                                      align={['center', 'center', 'center', 'center']}>
+                                    <Text tag='h2' type='cardTitle'>{e.title}</Text>
+                                    <Chart
+                                        loading={loader}
+                                        type={'line'}
+                                        border={'#000000'}
+                                        background={'#009aff'}
+                                        label={""}
+                                        dataSet={e.data}
+                                        labels={e.label}>
+                                    </Chart>
+                                </Flex>
+                            </Card>)
+                        }
+                    </Grid>
+                    :
                     <Card>
-                        <Flex layouts={['col']} justify={['center']} align={['center', 'center', 'center', 'center']}>
-                            <Text tag='h2' type='cardTitle'>TTFB</Text>
-                            {analytics.length &&
-                            <Chart
-                                loading={loader}
-                                type={'line'}
-                                border={'#000000'}
-                                background={'#009aff'}
-                                label={""}
-                                dataSet={analytics.map((e) => e.ttfbTime)}
-                                labels={analytics.map((e) => new Date(e.measurement_date).getUTCDate())}/>
-                            }
+                        <Flex layouts={['col']} justify={['center']}
+                              align={['center', 'center', 'center', 'center']}>
+                            <Text tag='h3'>There is no performance measurement in half an hour.</Text>
+                            <Text tag='h4'>Please visit link below or search for specific dates using time
+                                picker.</Text>
+                            <Link target={"_blank"} href={"https://performance-test-app.vercel.app/"}>TEST ME</Link>
                         </Flex>
                     </Card>
-
-                    <Card>
-                        <Flex layouts={['col']} justify={['center']} align={['center', 'center', 'center', 'center']}>
-                            <Text tag='h2' type='cardTitle'>FCP</Text>
-                            {analytics.length &&
-                            <Chart loading={loader} type={'line'}
-                                   border={'#000000'}
-                                   background={'#009aff'}
-                                   label={""}
-                                   dataSet={analytics.map((e) => e.fcpTime)}
-                                   labels={analytics.map((e) => new Date(e.measurement_date).getUTCDate())}/>}
-                        </Flex>
-                    </Card>
-
-                    <Card>
-                        <Flex layouts={['col']} justify={['center']} align={['center', 'center', 'center', 'center']}>
-                            <Text tag='h2' type='cardTitle'>DOM Load</Text>
-                            {analytics.length &&
-                            <Chart border={'#000000'}
-                                   background={'#009aff'}
-                                   loading={loader} type={'line'} label={""}
-                                   dataSet={analytics.map((e) => e.domLoadTime)}
-                                   labels={analytics.map((e) => new Date(e.measurement_date).getUTCDate())}/>}
-                        </Flex>
-                    </Card>
-
-                    <Card>
-                        <Flex layouts={['col']} justify={['center']} align={['center', 'center', 'center', 'center']}>
-                            <Text tag='h2' type='cardTitle'>Window Load</Text>
-                            {analytics.length &&
-                            <Chart loading={loader}
-                                   type={'line'}
-                                   border={'#000000'}
-                                   background={'#009aff'}
-                                   label={""}
-                                   dataSet={analytics.map((e) => e.windowLoadTime)}
-                                   labels={analytics.map((e) => new Date(e.measurement_date).getUTCDate())}/>}
-                        </Flex>
-                    </Card>
-                </Grid>
+                }
             </Container>
         </div>
     );
